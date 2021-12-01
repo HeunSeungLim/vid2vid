d79fdecb27d91b145efa3af3096f3c0b376cd18a
HeunSeungLim <37073913+HeunSeungLim@users.noreply.github.com>
2021-12-01 18:32:14 +0900

    Add files via upload

--- launch_docker.sh
+++ launch_docker.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 sudo nvidia-docker build -t vid2vid:CUDA9-py35 .
 
-sudo nvidia-docker run --rm -ti --ipc=host --shm-size 8G -v $(pwd):/vid2vid --workdir=/vid2vid vid2vid:CUDA9-py35 /bin/bash
+sudo nvidia-docker run --rm --gpus all -ti --ipc=host --shm-size 8G -v $(pwd):/vid2vid --workdir=/vid2vid vid2vid:CUDA9-py35 /bin/bash
